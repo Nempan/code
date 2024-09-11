1cf09d09f10d04b67cc63e71a1bbeac28755752a
nemo.sjostrom <nemo.sjostrom@elev.ga.ntig.se>
2024-09-11 13:50:07 +0200

    nästan klar

--- Elmo.py
+++ Elmo.py
@@ -1,32 +1,68 @@
 import random
+import time
 
 print("Spelaren börjar.")
 
-spelarvärde = 0
-dice = random.randint(1,6)
+spelarvärde1 = 0
+dator = 0
 dicecount = 0
+dicecountdator = 0
+svar = ""
+
+dice1 = random.randint(1,6)
+
+dice2 = random.randint(1,6)
+
+dice3 = dice1 + dice2 
+
+spelarvärde1 = dice3 + spelarvärde1
 
 while dicecount < 4:
-    dicecount += 1
-    spelarvärde += dice
-    print("Du har fått", spelarvärde, "poäng", ". runda:", dicecount)
-    
-
-    if spelarvärde == 14:
-        print("Du har vunnit!")
+    if spelarvärde1 > 14:
+                break
+    if svar == "Nej":
         break
-
-    elif dicecount <4:
-        print("Vill du slå igen")
-        svar = input("Ja eller Nej: ")
-        if svar == "Ja":
-            dice = random.randint(1,6)
-            spelarvärde += dice
-            print("Du har fått", spelarvärde, "poäng")
-        elif svar == "Nej":
-            print("Du har stannat på", spelarvärde, "poäng")
-            break
+    if dicecount >= 3:
+        break
+    dice = random.randint(1,6)
+    dicecount += 1
+    print("Du har fått", spelarvärde1, "poäng", ". runda:", dicecount)
     
+    while dicecount > 0 & dicecount < 3:
+        if spelarvärde1 > 14:
+                break
+        if svar == "Nej":
+            break
+        if dicecount >= 3:
+            print("Datorns tur!")
+            break
+        if spelarvärde1 == 14:
+            print("Du har vunnit!")
+            break
+        while dicecount < 3:
+            if spelarvärde1 > 14:
+                print("Du förlorade!")
+                break
+            print("Vill du slå igen")
+            svar = input("Ja eller Nej: ")
+            if svar == "Ja":
+                dice = random.randint(1,6)
+                spelarvärde1 += dice
+                dicecount += 1
+                print("Du har fått", spelarvärde1, "poäng")
+            elif svar == "Nej":
+                print("Du har stannat på", spelarvärde1, "poäng")
+                break
 
 
-    fe
+time.sleep(2)
+print("Datorn börjar")
+
+while dicecountdator < 4:
+    if dator == 14:
+        print("Datorn har vunnit!")
+        break
+    dicecountdator += 1
+
+
+
