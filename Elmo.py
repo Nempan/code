5e871c2647fda9d293c3a41fbbca890c93bd4d6d
nemo.sjostrom <nemo.sjostrom@elev.ga.ntig.se>
2024-09-13 09:37:03 +0200

    Klar (utan juice)

--- Elmo.py
+++ Elmo.py
@@ -2,6 +2,9 @@ import random
 import time
 
 print("Spelaren börjar.")
+time.sleep(1)
+
+målvärde = 14
 
 spelarvärde1 = 0
 dator = 0
@@ -30,6 +33,10 @@ while dicecount < 4:
     
     while dicecount > 0 & dicecount < 3:
         if spelarvärde1 > 14:
+                print("Du förlorade!")
+                time.sleep(3)
+                exit()
+        if spelarvärde1 > 14:
                 break
         if svar == "Nej":
             break
@@ -40,17 +47,19 @@ while dicecount < 4:
             print("Du har vunnit!")
             break
         while dicecount < 3:
-            if spelarvärde1 > 14:
-                print("Du förlorade!")
-                break
-            print("Vill du slå igen")
-            svar = input("Ja eller Nej: ")
-            if svar == "Ja":
+             if spelarvärde1 > 14:
+                  break
+             if spelarvärde1 == 14:
+                  break
+             print("Vill du slå igen")
+             svar = input("Ja eller Nej: ")
+             if svar == "Ja":
+                time.sleep(2)
                 dice = random.randint(1,6)
                 spelarvärde1 += dice
                 dicecount += 1
                 print("Du har fått", spelarvärde1, "poäng")
-            elif svar == "Nej":
+             elif svar == "Nej":
                 print("Du har stannat på", spelarvärde1, "poäng")
                 break
 
@@ -59,10 +68,33 @@ time.sleep(2)
 print("Datorn börjar")
 
 while dicecountdator < 4:
+    if dator > 10 and dator < 14:
+         break
+    if dator > 14:
+        print("Du vann!")
+        break
+    dice4 = random.randint(1,6)
     if dator == 14:
         print("Datorn har vunnit!")
         break
     dicecountdator += 1
+    while dator < 10 and dicecountdator < 4:
+         dice4 = random.randint(1,6)
+         dator += dice4
+         print("Datorn har fått", dator, "poäng")
+         if dator > 14:
+            break
+
+spelardiff = abs(spelarvärde1 - målvärde)
+
+datordiff = abs(dator - målvärde)
+
+if spelardiff < datordiff:
+    print("Du vann!")
+    
+elif spelardiff > datordiff:
+    print("Datorn vann!")
+
 
 
 
